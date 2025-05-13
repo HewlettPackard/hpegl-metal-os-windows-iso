c9b87bf01366be496537a330ddeb538b64170220
Mike Chuang <mike.chuang@hpe.com>
2025-05-13 11:38:02 -0500

    Undo No-IPPool changes until PROD portal picks up the required backend support.

--- Modules/Yaml.ps1
+++ Modules/Yaml.ps1
@@ -67,49 +67,49 @@ files:
 info:
   - encoding: base64
     templating: go-text-template
-    templating_input: hostdef-v4
+    templating_input: hostdef-v3
     target: vmedia-floppy
     path: /Autounattend.xml
     contents: $([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$CurrentPath/Autounattend.xml")))
   - encoding: base64
     templating: go-text-template
-    templating_input: hostdef-v4
+    templating_input: hostdef-v3
     target: vmedia-floppy
     path: /cloudbaseinit-setup.ps1
     contents: $([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$CurrentPath/glm-cloudbaseinit-setup.ps1.template.dos")))
   - encoding: base64
     templating: go-text-template
-    templating_input: hostdef-v4
+    templating_input: hostdef-v3
     target: vmedia-floppy
     path: /windowsexporter-setup.ps1
     contents: $([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$CurrentPath/glm-windowsexporter-setup.ps1.template.dos")))
   - encoding: base64
     templating: go-text-template
-    templating_input: hostdef-v4
+    templating_input: hostdef-v3
     target: vmedia-floppy
     path: /meta-data
     contents: $([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$CurrentPath/glm-meta-data.template.dos")))
   - encoding: base64
     templating: go-text-template
-    templating_input: hostdef-v4
+    templating_input: hostdef-v3
     target: vmedia-floppy
     path: /network-config
     contents: $([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$CurrentPath/glm-network-config.template.dos")))
   - encoding: base64
     templating: go-text-template
-    templating_input: hostdef-v4
+    templating_input: hostdef-v3
     target: vmedia-floppy
     path: /user-data
     contents: $([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$CurrentPath/glm-user-data.template.dos")))
   - encoding: base64
     templating: go-text-template
-    templating_input: hostdef-v4
+    templating_input: hostdef-v3
     target: vmedia-floppy
     path: /SetupComplete.cmd
     contents: $([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$CurrentPath/SetupComplete.cmd")))
   - encoding: base64
     templating: go-text-template
-    templating_input: hostdef-v4
+    templating_input: hostdef-v3
     target: vmedia-floppy
     path: /glm_finisher.ps1
     contents: $([System.Convert]::ToBase64String([System.IO.File]::ReadAllBytes("$CurrentPath/glm_finisher.ps1.template.dos")))
